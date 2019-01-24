d96f30a6b303f0a974cca9a3c634fe9b0cee81bc
Nathalia <nathreboucas5@gmail.com>
2019-01-24 18:47:13 -0300

    first commit

---
+++ src/main/java/com/api/pessoa/CrudPessoaApiApplication.java
@@ -0,0 +1,14 @@
+package com.api.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPessoaApiApplication.class, args);
+	}
+
+}
+
